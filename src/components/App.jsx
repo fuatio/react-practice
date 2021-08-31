991d7cf1db7b983077ce02933b5e44afb6bdf6d4
fuaticus <fuaticus@gmail.com>
2021-08-31 14:45:22 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card"
+import contacts from "../contacts"
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+
+
+function Card() {
+<div className="card">
+<div className="top">
+  <h2>Beyonce</h2>
+  <img
+    src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    alt="avatar_img"
+  />
+</div>
+<div className="bottom">
+  <p>+123 456 789</p>
+  <p>b@beyonce.com</p>
+</div>
+</div>
+}
