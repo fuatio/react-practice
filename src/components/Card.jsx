4b04a7f1636f05ce77f7183c2d609ca3cbb2018e
fuaticus <fuaticus@gmail.com>
2021-08-31 15:33:16 -0400

    completed exercise

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,22 @@
+import React from "react" 
 
-
-function Card() {
-<div className="card">
-<div className="top">
-  <h2>Beyonce</h2>
-  <img
-    src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-    alt="avatar_img"
-  />
-</div>
-<div className="bottom">
-  <p>+123 456 789</p>
-  <p>b@beyonce.com</p>
-</div>
-</div>
+function Card(props) {
+    return (
+    <div className="card">
+        <div className="top">
+            <h2 className='name'>{props.name}</h2>
+            <img
+            className="circle-img"
+                src={props.imgURL}
+                alt="avatar_img"
+            />
+        </div>
+        <div className="bottom">
+            <p className='info'>{props.phone}</p>
+            <p className='info'>{props.email}</p>
+        </div>
+    </div>
+    )
 }
+
+export default Card;
