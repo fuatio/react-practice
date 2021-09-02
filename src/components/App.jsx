ff800392df446249be47d0e38fe969d77a8350bf
fuaticus <fuaticus@gmail.com>
2021-09-01 23:01:19 -0400

    added some additional props features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Card from "./Card"
 import contacts from "../contacts"
+import Avatar from "./Avatar"
+import Detail from "./Detail"
 
 console.log(contacts)
 function App() {
   return (
     <div>
+      <Avatar
+        imgURL='https://ih1.redbubble.net/image.1922391619.7469/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
+      />
+      <Detail
+        detailInfo='partner'
+      /> 
       <h1 className="heading">My Contacts</h1>
       <Card
         name={contacts[0].name}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,23 @@
-import React from "react" 
+import React from "react"
+import Avatar from "./Avatar"
+import Detail from "./Detail"
 
 function Card(props) {
     return (
-    <div className="card">
-        <div className="top">
-            <h2 className='name'>{props.name}</h2>
-            <img
-            className="circle-img"
-                src={props.imgURL}
-                alt="avatar_img"
-            />
+        <div className="card">
+            <div className="top">
+                <h2 className='name'>{props.name}</h2>
+                <Avatar imgURL={props.imgURL} />
+            </div>
+            <div className="bottom">
+                <Detail
+                    detailInfo={props.phone}
+                />
+                <Detail
+                    detailInfo={props.email}
+                />
+            </div>
         </div>
-        <div className="bottom">
-            <p className='info'>{props.phone}</p>
-            <p className='info'>{props.email}</p>
-        </div>
-    </div>
     )
 }
 
