9bb11a51de8e1860e66f09a996f5829e5868cfc5
fuaticus <fuaticus@gmail.com>
2021-09-04 18:52:51 -0400

    starting point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import notes from "../notes.js"
 
-
-function App(props) {
-  return (
-    <div>
-      <Header />
-      {notes.map(x => (
-        <Note
-          key={x.key}
-          title={x.title}
-          content={x.content}
-        />
-      ))}
-      <Footer />
-    </div>
-  );
+function App() {
+  return <div />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+ReactDOM.render(
+  <div className="container">
+    <h1>0</h1>
+    <button>+</button>
+  </div>,
+  document.getElementById("root")
+);
