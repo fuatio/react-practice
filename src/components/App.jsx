d395f7cb40659d35309718f298f371e131d50a97
fuaticus <fuaticus@gmail.com>
2021-09-05 14:01:01 -0400

    completed exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+
 
 function App() {
+
+  let [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function refreshTime() {
+    setTime(new Date().toLocaleTimeString())
+  }
+
+  setInterval(refreshTime, 1000)
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={refreshTime}>Get Time</button>
+
     </div>
   );
 }
