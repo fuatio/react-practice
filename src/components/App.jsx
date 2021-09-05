0f02d782b62834a8168a2dbbc987d3b338142d7e
fuaticus <fuaticus@gmail.com>
2021-09-05 13:11:44 -0400

    beginning state of exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-
-  const [count, setCount] = useState(0)
-
-  function increase() {
-    setCount(count + 1)
-  }
-
-  function decrease() {
-    setCount(count - 1)
-  }
-
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={decrease}>-</button>
-      <button onClick={increase}>+</button>
+      <h1>TIME</h1>
+      <button>Get Time</button>
     </div>
-  )
+  );
 }
 
 export default App;
