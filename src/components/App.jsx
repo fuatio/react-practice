6ad0dbfb930848482bb7919e8a28951d3347c0a9
fuaticus <fuaticus@gmail.com>
2021-09-07 13:44:27 -0400

    completed exercise to update event as typing into form input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 
 function App() {
 
-    const [firstName, setFirstName] = useState("")
+    const [fName, setFName] = useState("")
+    const [lName, setLName] = useState("")
 
-    function handleFirstName() {
-        useState(setFirstName)
+    function updateFName(event) {
+        const firstName = event.target.value
+        setFName(firstName)
     }
 
+    function updateLName(event) {
+        const lastName = event.target.value
+        setLName(lastName)
+    }
 
     return (
         <div className="container">
-            <h1>Hello {firstName} </h1>
+            <h1>Hello {fName} {lName} </h1>
             <form>
-                <input name="fName" placeholder="First Name" value={handleFirstName} />
-                <input name="lName" placeholder="Last Name" />
+                <input name="fName" onChange={updateFName} placeholder="First Name" value={fName} />
+                <input name="lName" onChange={updateLName} placeholder="Last Name" value={lName} />
                 <button>Submit</button>
             </form>
         </div>
