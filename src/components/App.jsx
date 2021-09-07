c68a7b43d115c84384cafb476951c5f9d26f08fa
fuaticus <fuaticus@gmail.com>
2021-09-07 13:26:31 -0400

    tryin exercise practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,22 @@ import React, { useState } from "react";
 
 function App() {
 
-    const [name, setName] = useState("");
-    const [headingText, setHeading] = useState("")
+    const [name, firstName] = useState("")
 
-    function handleChange(event) {
-        setName(event.target.value)
+    function setFirstName() {
+        useState(firstName)
     }
 
-    function handleClick() {
-        setHeading(name);
-    }
 
     return (
         <div className="container">
-            <h1>Hello {headingText}</h1>
-            <form onSubmit={handleClick}>
-                <input
-                    onChange={handleChange}
-                    type="text"
-                    placeholder="What's your name?"
-                    value={name}
-                />
+            <h1>Hello {setFirstName} </h1>
+            <form>
+                <input name="fName" placeholder="First Name" />
+                <input name="lName" placeholder="Last Name" />
                 <button>Submit</button>
             </form>
-        </div >
+        </div>
     );
 }
 
