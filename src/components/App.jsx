796129f91c29b656290bee5918774419b45ccdc9
fuaticus <fuaticus@gmail.com>
2021-09-07 15:36:08 -0400

    beginning state of exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,26 @@
 import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
 
-    const [fullName, setFullName] = useState({
-        fName: "",
-        lName: ""
-    })
-
-    function handleChange(event) {
-
-        const {value, name} = event.target;
-
-        setFullName((prevValue) => {
-            if (name === 'fName') {
-                return {
-                    fName: value,
-                    lName: prevValue.lName
-                }
-            } else if (name === 'lName') {
-                return {
-                    fName: prevValue.fName,
-                    lName: value
-                }
-            }
-        })
-    }
-
-    return (
-        <div className="container">
-            <h1>Hello {fullName.fName} {fullName.lName} </h1>
-            <form>
-                <input name="fName"
-                    onChange={handleChange}
-                    placeholder="First Name"
-                    value={fullName.fName}
-                />
-                <input name="lName"
-                    onChange={handleChange}
-                    placeholder="Last Name"
-                    value={fullName.lName}
-                />
-                <button>Submit</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <input name="email" placeholder="Email" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
