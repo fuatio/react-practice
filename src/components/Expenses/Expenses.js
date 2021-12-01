9eb90d259f63a5b4e8788d2743c2a6fa72332c37
fuaticus <fuaticus@gmail.com>
2021-11-30 22:10:01 -0500

    completed assignment 4.60, added child component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,32 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card'
 import "./expenses.css"
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react/cjs/react.development";
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
-        <Card className='expenses'>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-        </Card >
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                />
+            </Card >
+        </div>
     )
 }
 
