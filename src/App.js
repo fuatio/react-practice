8a5b70c3870dcf63759f1524e38fb33c73e82a24
fuatio <fuaticus@gmail.com>
2021-12-07 18:53:20 -0500

    completed app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}]
-  })}
+      return [...prevUsersList, {
+        name: uName,
+        age: uAge,
+        id: Math.random().toString()
+      }]
+    })
+  }
 
   return (
     <div>
